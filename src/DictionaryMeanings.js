830a75abf4243a1fe856d4359bb37b21d0aa3912
chardi90 <chardi90@hotmail.com>
2025-07-11 17:41:45 +0100

    Added the manifest back in + added synonyms component
    - recreated manifest doc
    - created synonyms component in dictionaryMeanings
    - reordered dictionaryResults components
    - added index numbers to list on dictionaryMeanings

--- src/DictionaryMeanings.js
+++ src/DictionaryMeanings.js
@@ -1,22 +1,35 @@
 import React from "react";
 
 export default function Meanings({ partOfSpeech, meanings }) {
+  const synonyms = meanings
+    .flatMap((entry) => entry.synonyms || [])
+    .filter(Boolean)
+    .join(", ");
+
   return (
     <div className="Meaning">
+      {synonyms && (
+        <p>
+          <strong>Synonyms:</strong> {synonyms}
+        </p>
+      )}
+
       <h3>{partOfSpeech}</h3>
       <ol>
-        {meanings.map((entry, i) => (
-          <li key={i}>
-            {entry.definition}
-            {entry.example && (
-              <ul>
-                <li>
-                  <em>{entry.example}</em>
-                </li>
-              </ul>
-            )}
-          </li>
-        ))}
+        {meanings.map((entry, i) =>
+          entry.definitions?.map((def, j) => (
+            <li key={`${i}-${j}`}>
+              {def.definition}
+              {def.example && (
+                <ul>
+                  <li>
+                    <em>{def.example}</em>
+                  </li>
+                </ul>
+              )}
+            </li>
+          ))
+        )}
       </ol>
     </div>
   );
--- src/DictionaryResults.js
+++ src/DictionaryResults.js
@@ -4,20 +4,34 @@ import Meanings from "./DictionaryMeanings";
 export default function Dictionary(props) {
   if (!props.results) return null;
 
-  const groupedMeanings = props.results.meanings.reduce((acc, meaning) => {
+  const groupedMeanings = {};
+
+  props.results.meanings.forEach((meaning) => {
     const part = meaning.partOfSpeech;
 
-    if (!acc[part]) {
-      acc[part] = [];
+    if (!groupedMeanings[part]) {
+      groupedMeanings[part] = [];
     }
 
-    acc[part].push(meaning);
-    return acc;
-  }, {});
+    groupedMeanings[part].push({
+      definitions: [
+        {
+          definition: meaning.definition,
+          example: meaning.example,
+        },
+      ],
+      synonyms: meaning.synonyms || [],
+    });
+  });
+
+  const partsOfSpeechList = [
+    ...new Set(props.results.meanings.map((m) => m.partOfSpeech)),
+  ].join(", ");
 
   return (
     <div className="Dictionary">
       <h1>{props.results.word}</h1>
+      <h2>{partsOfSpeechList}</h2>
       <p>{props.results.phonetic}</p>
       {Object.keys(groupedMeanings).map((part, index) => (
         <div key={index}>
