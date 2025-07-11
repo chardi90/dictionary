d28ecce2b4d58d77632e5d486a8ef21f08ae9445
chardi90 <chardi90@hotmail.com>
2025-07-11 13:51:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form className="search-form">
+          <input type="search"></input>
+        </form>
+      </header>
+      <main>
+        <h1 className="text-center">Dictionary</h1>
+        <h2 className="text-center">Knowledge at your fintertips!</h2>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
