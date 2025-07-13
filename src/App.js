184bd037f73ed3e36ab32ab5618d1e052eec3c99
chardi90 <chardi90@hotmail.com>
2025-07-13 12:40:20 +0100

    Styled splash page
    - Created word of the day box & hard coded content
    - Styled margins and padding for container, sections, header and footer

--- src/App.js
+++ src/App.js
@@ -20,8 +20,28 @@ export default function App() {
         <main>
           {!results && (
             <>
-              <h1 className="text-center">Dictionary</h1>
-              <h2 className="text-center">Knowledge at your fingertips!</h2>
+              <div>
+                <div className="Dictionary-title">
+                  <h1 className="text-center">Dictionary</h1>
+                  <h2 className="text-center">Knowledge at your fingertips!</h2>
+                </div>
+                <div className="word-of-day">
+                  <div className="day-wrapper">
+                    <h5>Word of the Day</h5>
+                    <p>13 July, 2025</p>
+                    <h6>book</h6>
+                    <p>
+                      <em>part</em> | phonetic
+                    </p>
+                    <div className="word-wrapper">
+                      <h2>What it Means</h2>
+                      <p>
+                        <strong>definition</strong> ~ example
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           )}
           <Dictionary results={results} />
